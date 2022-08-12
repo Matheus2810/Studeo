428bac61e3ecb2c14413aed7fce003e38d4f8b7b
Matheus Batista <macedo2004.mn@gmail.com>
2022-08-12 18:26:58 -0300

    tcc alteracoes novas

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function linkMateria(link){
     window.location.href = link;
 }
+
+function mostrarNotificacao(id){
+    var div = $('#'+id);
+
+    if(div.hasClass('invisivel')){
+        div.removeClass('invisivel');
+    }else{
+        div.addClass('invisivel');
+    }
+}
